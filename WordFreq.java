cfd0690e8cbe84a692625b481110faf5813e2f7f
steph-cu <62310496+steph-cu@users.noreply.github.com>
2020-10-14 16:23:03 -0600

    Update WordFreq.java

--- WordFreq.java
+++ WordFreq.java
@@ -1,48 +1,4 @@
-class WordFreq implements Comparable<WordFreq> {
-	public WordFreq(String lang){
-		word = lang; 
-		freq = 0;
-	}
-	private Integer freq;// stores how many times its been used
-	private String word;// store the word
-	private matchCase{// how well it matches
-		public matchCase(){
-			matches = {"0"} ;
-			off = {0}; 
-		}
-		public matchCase(String mat, int offby, int place){
-			matches[place] = mat;
-			off = offby;
-		}
-		String[10] matches;
-		int[10] off; 
-		public void printMatches(){
-			for(int i = 0; i < 10; i++){
-				System.out.print(matches[i] + "(" + off[i] + "), ");
-			}
-		}
-	}
-	
-	
-	@Override
-	public int compareTo(WordFreq cWord){
-		return this.word.compareTo(cWord.word)
-	}
-	
-	public void incFreq(){
-		this.freq++; 
-	}
-	public void findClose(AvlNode<WordFreq> curr){
-		if (curr == null) return;//returns nothing
-		cleft = curr.left;// probably needs changing along with the parameter
-		cright = curr.right;
-		int distance = minDistance(this.word, curr.elemtent);// to get distance between this word and the current dic word 
-		for (int i = 0; i < 10; i++){// goes through the " close matches list " 
-			if (this.matchCase.off[i] == 0 || this.matchCase.off[i] > distance){// if it hasn't been filled or the distance of the current dic word is shorter
-				curr.element.matches[i] = curr.element.word;// the place in matches will be filled with current dic word 
-				break;// and out of loop it goes 
-			} 
-		}public class WordFreq implements Comparable<WordFreq> {
+public class WordFreq implements Comparable<WordFreq> {
 	public WordFreq(String lang){
 		word = lang; 
 		freq = 0;
@@ -70,7 +26,8 @@ class WordFreq implements Comparable<WordFreq> {
 	
 	@Override
 	public int compareTo(WordFreq cWord){
-		return this.word.compareTo(cWord.word);
+		//return this.word.compareTo(cWord.word);
+		return this.freq.compareTo(cWord.freq);
 	}
 	
 	public void incFreq(){
@@ -116,22 +73,16 @@ class WordFreq implements Comparable<WordFreq> {
         //System.out.println(dp[len1][len2]);
         return dp[len1][len2];
     }
-	public void findClose(AvlNode<WordFreq> curr){
-		if (curr == null) return;//returns nothing
-		cleft = curr.left;// probably needs changing along with the parameter
-		cright = curr.right;
-		int distance = minDistance(this.word, curr.elemtent);// to get distance between this word and the current dic word 
-		for (int i = 0; i < 10; i++){// goes through the " close matches list " 
-			if (this.matchCase.off[i] == 0 || this.matchCase.off[i] > distance){// if it hasn't been filled or the distance of the current dic word is shorter
-				curr.element.matches[i] = curr.element.word;// the place in matches will be filled with current dic word 
-				break;// and out of loop it goes 
-			} 
+	public void findClose(List<WordFreq> list){// replace for going through a list of the dictionary
+		for(int j = 0; j < list.length; j++){
+			int distance = minDistance(this.word, list.get(j));// to get distance between this word and the current dic word 
+			for (int i = 0; i < 10; i++){// goes through the " close matches list " 
+				if (this.matchCase.off[i] == 0 || this.matchCase.off[i] > distance){// if it hasn't been filled or the distance of the current dic word is shorter
+					this.matchCase.matches[i] = list.get(j);// the place in matches will be filled with current dic word 
+					this.matchCase.off[i] = distance; 
+					break;// and out of loop it goes 
+				} 
+			}
 		}
-		findClose(cleft);// goes to left
-		findClose(cright);	// goes to right
-	}
-}
-		findClose(cleft);// goes to left
-		findClose(cright);	// goes to right
 	}
 }
